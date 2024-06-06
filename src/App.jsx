3d5091e981bff271aaa8e46294217db5f07d57fd
Pooria <pooria_rahimi@comp.iust.ac.ir>
2024-06-06 14:51:19 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+import { Routes, Route, Navigate,useLocation } from 'react-router-dom';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+  
+    </>
+  )
+}
+
+export default App
